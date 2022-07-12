f96ef03b5168987ecb1be62bce1334b660e2763a
Timur <tim.aivarov@gmail.com>
2022-07-12 17:51:37 +0600

    create simple date deserializer

---
+++ src/test/java/com/example/helpers/DateDeserializer.java
@@ -0,0 +1,22 @@
+package com.example.helpers;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateDeserializer extends JsonDeserializer<LocalDateTime> {
+
+    @Override
+    public LocalDateTime deserialize(JsonParser jsonParser,
+                                     DeserializationContext deserializationContext)
+            throws IOException {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter
+                .ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        return LocalDateTime.parse(jsonParser.getText(), dateTimeFormatter);
+    }
+}
